2e47a1082b930efc5cbc1611a85f11a122d9a150
Victor Hugo <80632092+Hugoritimo@users.noreply.github.com>
2024-09-27 16:55:44 -0300

    Primeiro Commit

---
+++ ged-backend/src/main/java/com/example/ged_backend/GedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ged_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GedBackendApplication.class, args);
+	}
+
+}
